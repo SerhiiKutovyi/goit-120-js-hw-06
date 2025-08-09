4b693cd370bcf0e450ba480203968755fe086d31
SerhiiKutovyi <wagenrex@gmail.com>
2025-08-09 18:25:51 +0300

    hw-1

---
+++ js/task-1.js
@@ -0,0 +1,25 @@
+// const pizzaPalace = {
+//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
+
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+// };
+
+// console.log(pizzaPalace.checkPizza('Smoked'));
+
+function foo() {
+  console.log(this);
+}
+
+foo();
