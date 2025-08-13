b7f8eb570c2bd3bf0d84222f8c15b6f1e0ca9f5a
SerhiiKutovyi <wagenrex@gmail.com>
2025-08-13 18:02:50 +0300

    hw-1

--- js/task-1.js
+++ js/task-1.js
@@ -80,3 +80,131 @@
 // console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
 // console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
 // console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
+
+// class Car {}
+// const qwe = new Car();
+// console.log(qwe);
+
+// class User {
+//   constructor() {
+//     console.log('constructor call');
+//   }
+// }
+
+// const mango = new User();
+// console.log(mango);
+
+// class User {
+//   constructor(name, email) {
+//     console.log(name, email);
+//   }
+// }
+
+// const mango = new User('Mango', 'mango@mail.com');
+// console.log(mango);
+
+// class User {
+//   constructor(name, email) {
+//     // Ініціалізація властивостей екземпляра
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// const mango = new User('Mango', 'mango@mail.com');
+// console.log(mango);
+
+// const poly = new User('Poly', 'poly@mail.com');
+// console.log(poly);
+
+// class Car {
+//   constructor(params) {
+//     this.brand = params.brand;
+//     this.model = params.model;
+//     this.price = params.price;
+//   }
+// }
+
+// console.log(new Car({ brand: 'Audi', model: 'Q3', price: 36000 }));
+// console.log(new Car('BMW', 'X5', 58900));
+// console.log(new Car('Nissan', 'Murano', 31700));
+
+// class User {
+//   constructor(params) {
+//     this.name = params.name;
+//     this.email = params.email;
+//   }
+
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   getName() {
+//     return this.name;
+//   }
+
+//   changeEmail(newName) {
+//     this.name = newName;
+//   }
+// }
+
+// const mango = new User({
+//   name: 'Mango',
+//   email: 'mango@mail.com',
+// });
+
+// // mango.changeEmail('Kiwi');
+
+// console.log(mango.getEmail(), mango.getName());
+
+// class Car {
+//   constructor(params) {
+//     this.brand = params.brand;
+//     this.model = params.model;
+//     this.price = params.price;
+//   }
+
+//   getPrice() {
+//     return this.price;
+//   }
+
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+// }
+
+class Car {
+  #brand;
+  #model;
+  #price;
+
+  constructor(params) {
+    this.#brand = params.brand;
+    this.#model = params.model;
+    this.#price = params.price;
+  }
+
+  get brand() {
+    return this.#brand;
+  }
+
+  set brand(newBrand) {
+    this.#brand = newBrand;
+  }
+
+  get model() {
+    return this.#model;
+  }
+
+  set model(newModel) {
+    this.#model = newModel;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    this.#price = newPrice;
+  }
+}
